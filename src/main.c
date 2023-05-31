1c0d67fefb1c19e771586fecc4997e39f3b727ee
JoaoRafa19 <joaopedrorafael19@gmail.com>
2023-05-31 17:26:22 -0300

    Correção pesquisa vazia

--- src/main.c
+++ src/main.c
@@ -25,13 +25,10 @@ void imprimePatriciaGTK(PATRICIA P)
 {
     if (P == NULL)
         return;
-
     if (P->tipo == interno)
         imprimePatriciaGTK(P->no.Ninterno.esq);
-
     if (P->tipo == externo)
         printf("%s\n", P->no.Nexterno.string);
-
     if (P->tipo == interno)
         imprimePatriciaGTK(P->no.Ninterno.dir);
 }
@@ -39,9 +36,7 @@ void imprimePatriciaGTK(PATRICIA P)
 int main(int argc, char *argv[])
 {
     Screen *widgets = g_slice_new(Screen);
-
     gtk_init(&argc, &argv);
-
     widgets->builder = gtk_builder_new();
     if (gtk_builder_add_from_file(widgets->builder, "../glade/interface.glade", &widgets->error) == 0)
     {
@@ -49,7 +44,6 @@ int main(int argc, char *argv[])
         g_clear_error(&widgets->error);
         return 1;
     }
-
     widgets->window = GTK_WIDGET(gtk_builder_get_object(widgets->builder, "window"));
     leArquivo(&widgets->patricia, "entrada.txt");
     widgets->lb_message = GTK_WIDGET(gtk_builder_get_object(widgets->builder, "label"));
@@ -57,10 +51,8 @@ int main(int argc, char *argv[])
     widgets->button = GTK_WIDGET(gtk_builder_get_object(widgets->builder, "button"));
     g_signal_connect(widgets->button, "clicked", G_CALLBACK(get_text), widgets);
     g_signal_connect(widgets->window, "destroy", G_CALLBACK(quit), widgets);
-
     gtk_widget_show_all(widgets->window);
     gtk_main();
-
     return 0;
 }
 
@@ -78,13 +70,10 @@ static void get_text(GtkWidget *widget,
                      gpointer data)
 {
     Screen *widgets = (Screen *)data;
-
     const char *text = gtk_entry_get_text(widgets->entry);
     String termos = (String) malloc(strlen(text)*sizeof(char));
     strcpy(termos, text);
-
     int c = countChars(termos, ' ') +1;
-
     int i = 0;
     char *p = strtok(termos, " ");
     char *array[c];
@@ -96,7 +85,6 @@ static void get_text(GtkWidget *widget,
     int nFiles;
     String files = getFiles("entrada.txt", NULL, &nFiles);
     String result;
-
     gchar *display;
     if(strcmp("", termos)==0){
         return;
@@ -105,8 +93,6 @@ static void get_text(GtkWidget *widget,
     display = g_strdup_printf("%s", result);  
     gtk_label_set_text (GTK_LABEL(widgets->lb_message), display); //set label to "display"
     g_free(display);     
-    
-    //gtk_label_set_label(GTK_LABEL(widgets->lb_message), result);
 }
 
 int countChars(char *s, char c)
